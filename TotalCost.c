4ff457e10e6272c7c2eef1a111bf058291e81a81
reaganclever <kasulereagan86@gmail.com>
2025-06-08 13:52:49 +0300

    TTWWC2-6 COMMIT#1 ON BRANCH#5

--- TotalCost.c
+++ TotalCost.c
@@ -4,14 +4,14 @@
 
 double computeTotalCost(){
 
-const double taxRate = 0.03;
-double productPrice = 35000;
+const double taxRate = 0.06;
+double productPrice = 40000;
 double totalCost = productPrice + (taxRate * productPrice);
 
-printf("\nPRODUCT PRICE:\t%.2lf\n" , productPrice);
-printf("TAX RATE:\t%.2lf\n" , taxRate);
+printf("\nNEW PRODUCT PRICE:\t%.2lf\n" , productPrice);
+printf("NEW TAX RATE:\t%.2lf\n" , taxRate);
 printf("------------\n");
-printf("TOTAL COST:\t%.2lf\n" , totalCost);
+printf(" NEW TOTAL COST:\t%.2lf\n" , totalCost);
 
 return totalCost;
 
